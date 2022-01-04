59f158f669a02f8f1b51e58d00c1218bd701d001
Ethan M Pham <84094339+emp86472@users.noreply.github.com>
2022-01-04 17:24:09 -0500

    Delete Config.java

--- src/main/java/pham/config/Config.java
+++
@@ -1,9 +0,0 @@
-package pham.config;
-
-public final class Config {
-    private Config() {}
-    //package private default
-    public static final String BOT_TOKEN = "OTE0Mzk1NTQzMTQzMzMzODg4.YaMbNg.GY2ICsiM0ZXl_cVSjfjBaY5zSCY";
-    public static final String TEST_SERVER_ID = "197431852225527809";
-    public static final boolean TEST_MODE = false;
-}
