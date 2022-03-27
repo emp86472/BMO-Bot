a81634d7a40017abe20b589210269df9ff749a07
Ethan Pham <ethanpham001@gmail.com>
2022-03-27 16:42:05 -0400

    move simple responses to its own class (untracked)

--- src/main/java/pham/bmo/MessageListener.java
+++ src/main/java/pham/bmo/MessageListener.java
@@ -26,8 +26,6 @@ public class MessageListener extends ListenerAdapter {
         if (event.getMessage().getGuild().getId().equals(TEST_SERVER_ID) != TEST_MODE) return;
         if (event.getAuthor().isBot()) return;
 
-        String[] emoticon = {":3", ":)", ":D", "!"};
-
         String keyword = event.getMessage().getContentRaw();
         keyword = keyword.toLowerCase();
         String[] token = keyword.split("\\s");
@@ -38,28 +36,7 @@ public class MessageListener extends ListenerAdapter {
                 command[i].execute(event);
             } //for
         } else {
-            if (event.getAuthor().getAsTag().equals("synrg#1396")) return;
-            if (keyword.contains("candies")) {
-                event.getChannel().sendMessage("Candies nuts fit in your mouth!? XD").queue();
-            } else if (keyword.contains("gargalon")) {
-                event.getChannel().sendMessage("Gargalon these balls lmao").queue();
-            } else if (keyword.contains("imagine dragons")) {
-                event.getChannel().sendMessage("Imagine dragon these balls all over your face! HA").queue();
-            } else if (keyword.contains("candice")) {
-                event.getChannel().sendMessage("Candice dick fit in your mouth lol").queue();
-            } else if (keyword.contains("kansas")) {
-                event.getChannel().sendMessage("Kansas dick fit in your mouth? LOL").queue();
-            } else if (keyword.contains("joe")) {
-                event.getChannel().sendMessage("joe mama xd").queue();
-            } else if (keyword.contains("sugondese")) {
-                event.getChannel().sendMessage("sugondese NUTS lmaoooo").queue();
-            } else if (keyword.contains("ligma")) {
-                event.getChannel().sendMessage("ligma balls heh").queue();
-            } else if (keyword.contains("liek")) {
-                event.getChannel().sendMessage("You misspelled 'like' " + emoticon[rand.nextInt(emoticon.length)]).queue();
-            } else if (keyword.contains("i love you")) {
-                event.getMessage().reply("Geez, get a room .-.").queue();
-            } //if
+            SimpleResponse.respond(event, keyword);
         } //if
     } //onMessageReceived
 
